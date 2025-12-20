977b6e9274d9ef3f3ba75a6371ee399484b963e9
Omole Daniel <danielomole18@gmail.com>
2025-12-20 22:38:02 +0100

    ensure the increment of likes

--- src/modules/post/post.controller.ts
+++ src/modules/post/post.controller.ts
@@ -90,7 +90,10 @@ export const getUserPosts = async (req: Request, res: Response) => {
       where: { authorId: { in: ids } },
       orderBy: [{ createdAt: "desc" }, { id: "desc" }],
       take: limit + 1,
-      include: { author: { select: { id: true, username: true, name: true } } },
+      include: {
+        author: { select: { id: true, username: true, name: true } },
+        _count: { select: { likes: true } },
+      },
     };
 
     if (cursor) {
@@ -138,7 +141,10 @@ export const getPostById = async (req: Request, res: Response) => {
 
     const post = await prisma.post.findUnique({
       where: { id },
-      include: { author: { select: { id: true, username: true, name: true } } },
+      include: {
+        author: { select: { id: true, username: true, name: true } },
+        _count: { select: { likes: true } },
+      },
     });
     if (!post) return res.status(404).json({ error: "Post not found" });
 
